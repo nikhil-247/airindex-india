2a63c6e50562fe866fd8d1f937ceecf4816cb5f0
Nikhil Kushwaha <141046305+nikhil-247@users.noreply.github.com>
2026-09-09 03:14:27 +0530

    fix: satisfy import ordering in alembic

--- alembic/env.py
+++ alembic/env.py
@@ -2,11 +2,10 @@ import asyncio
 from logging.config import fileConfig
 from typing import Any
 
+from airindex.config import get_settings
 from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
-from airindex.config import get_settings
 
 config = context.config
 if config.config_file_name:
